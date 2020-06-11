ad1551942f578071de37d831c5e30fb366ca92ab
yurakohut <yura.kohut@teamvoy.com>
2020-06-11 13:19:53 +0300

    added spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-lf7d51q4a7">
+      <div class="ldio-kfdvxus9b6a">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
