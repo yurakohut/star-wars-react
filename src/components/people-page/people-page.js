59d7bdf32ac1b181df2d8d304f8270b8eeb79ddd
yurakohut <yura.kohut@teamvoy.com>
2020-06-12 23:16:29 +0300

    added error-boundry

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -3,48 +3,35 @@ import ItemList from "./../item-list/item-list";
 import PersonDetails from "./../person-details/person-details";
 import ErrorIndicator from "../error-indicator/error-indicator";
 import SwapiService from "./../../services/swapi-service";
-
-const Row = ({ left, right }) => {
-  return (
-    <div className="row mb2">
-      <div className="col-md-6">{left}</div>
-      <div className="col-md-6">{right}</div>
-    </div>
-  );
-};
+import Row from "./../row/row";
+import ErrorBoundry from './../error-boundry/error-boundry';
 export default class PeoplePage extends Component {
   swapiService = new SwapiService();
   state = {
     selectedPerson: 5,
-    hasError: false,
   };
 
   onPersonSelected = (id) => {
     this.setState({ selectedPerson: id });
   };
 
-  componentDidCatch() {
-    this.setState({
-      hasError: true,
-    });
-  }
-
   render() {
-    const { selectedPerson, hasError } = this.state;
-    if (hasError) {
-      return <ErrorIndicator />;
-    }
+    const { selectedPerson } = this.state;
+
     const itemList = (
       <ItemList
         onItemSelected={(id) => this.onPersonSelected(id)}
         getData={this.swapiService.getAllPeople}
-        renderItem={({ name, gender, birthYear }) =>
-          `${name} (${gender}) - ${birthYear}`
-        }
-      />
+      >
+        {(i) => `${i.name} - ${i.birthYear}`}
+      </ItemList>
     );
 
-    const personDetails = <PersonDetails personId={selectedPerson} />;
+    const personDetails = (
+      <ErrorBoundry>
+        <PersonDetails personId={selectedPerson} />
+      </ErrorBoundry>
+    );
     return <Row left={itemList} right={personDetails} />;
   }
 }
